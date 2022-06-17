4a7faecc8a486411eec8d56dfd3b9bc7181cf913
Kuzzou <72271883+Kuzzou@users.noreply.github.com>
2022-06-17 22:34:50 +0100

    Delete Short Walk directory

--- Short Walk/src/Interactable/InteractableItems.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-class_name InteractableItem
-
-#By default interactable items are only available to the Player class
-func interaction_can_interact(interactionComponentParent: Node) -> bool:
-	return interactionComponentParent is Player
